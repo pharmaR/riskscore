8538f956534d9ea68e9775eb4f240b3f23c9acc8
AARON-CLARK <clark.aaronchris@gmail.com>
2023-07-19 07:31:56 -0400

    add placeholder vignette and initial spelling test

--- dev/pkg_setup.R
+++ dev/pkg_setup.R
@@ -10,3 +10,14 @@ usethis::create_package(getwd())
 usethis::use_lifecycle_badge( "Experimental" ) #Experimental, Maturing, Stable, Superseded, Archived, Dormant, Questioning
 usethis::use_news_md( open = TRUE )
 usethis::use_mit_license("R Validation Hub")
+
+# Add package dependencies to import statement
+# usethis::use_package("[pkg name]") # not run yet
+
+# Add data-raw folder to generate exported data objects
+usethis::use_data_raw(name = "", open = FALSE )
+
+
+# Documentation
+## Vignettes
+usethis::use_vignette("riskscore")
---
+++ tests/testthat/test-spelling.R
@@ -0,0 +1,10 @@
+
+if(requireNamespace('spelling', quietly = TRUE))
+  spelling::spell_check_test(vignettes = TRUE, error = TRUE,
+                             skip_on_cran = TRUE)
+
+# To check for spelling mistakes:
+# spelling::spell_check_package()
+
+# To add remainging (un-correctable) words to the wordlist:
+# spelling::update_wordlist()
