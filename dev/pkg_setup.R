1b4cf77da066375bde796323e4302a97c69f59d0
AARON-CLARK <clark.aaronchris@gmail.com>
2023-07-19 07:21:13 -0400

    manually update description with some initial info

--- dev/pkg_setup.R
+++ dev/pkg_setup.R
@@ -1,4 +1,12 @@
+
 # Workflow for setting up the repository
+
+# create pkg skeleton structure
 usethis::create_package(getwd())
+
+# Manually modify the description ----------------------------------------------
+
+# Add a bunch of necessities
 usethis::use_lifecycle_badge( "Experimental" ) #Experimental, Maturing, Stable, Superseded, Archived, Dormant, Questioning
 usethis::use_news_md( open = TRUE )
+use_mit_license("My Name")
