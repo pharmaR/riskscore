3b4105d7c156788475bd9878c8e15f3140641e11
AARON-CLARK <clark.aaronchris@gmail.com>
2023-07-19 07:09:52 -0400

    run usethis::create_package() and create 'dev' folder

---
+++ dev/pkg_setup.R
@@ -0,0 +1,4 @@
+# Workflow for setting up the repository
+usethis::create_package(getwd())
+usethis::use_lifecycle_badge( "Experimental" ) #Experimental, Maturing, Stable, Superseded, Archived, Dormant, Questioning
+usethis::use_news_md( open = TRUE )
