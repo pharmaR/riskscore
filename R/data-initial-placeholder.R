cd5f173e8d3d5e8761380f41ae4f6ba6a070ab79
AARON-CLARK <clark.aaronchris@gmail.com>
2023-07-20 15:12:12 -0400

    add more appropriate variable descriptions to help docs

--- R/data-initial-placeholder.R
+++ R/data-initial-placeholder.R
@@ -9,6 +9,16 @@
 #'   (one row per package) and 23 variables, 18 of which are riskmetric
 #'   assessments.
 #'
+#' \describe{
+#'   \item{package}{The name of a R package hosted on CRAN}
+#'   \item{version}{The package's version number}
+#'   \item{riskmetric_run_date}{The date riskmetric was run}
+#'   \item{riskmetric_version}{The version of riskmetric used to derive scores}
+#'   \item{pkg_score}{The package's riskmetric score}
+#' }
+#' All other variables correspond to the riskmetric assessments that exist at
+#'   the time of execution. Please refer to riskmetric's documentation for more
+#'   info.
 #'
 #' @return A data.frame
 "v0.2.1_cran_20230621"
