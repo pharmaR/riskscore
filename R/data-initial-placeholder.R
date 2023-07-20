cc2f8476400a1d6be39d631899d57ad9e1952a48
Aaron Clark <CLARK.AARONCHRIS@GMAIL.COM>
2023-07-20 13:11:29 -0400

    Update R/data-initial-placeholder.R
    Co-authored-by: Doug Kelkhoff <18220321+dgkf@users.noreply.github.com>

--- R/data-initial-placeholder.R
+++ R/data-initial-placeholder.R
@@ -3,9 +3,12 @@
 #'
 #' The initial data.frame is generated as a placeholder until a more streamlined
 #' process is implemented. It contains the initial run where all of CRAN was
-#' scored using riskmetric v0.2.1 on 2023-06-21. It contains 19,715 observations
-#' (one row per package) and 24 variables, 18 of which are riskmetric
-#' assessments.
+#' scored using riskmetric v0.2.1 on 2023-06-21.
+#'
+#' @format A `data.frame` containing 19,715 observations
+#'   (one row per package) and 24 variables, 18 of which are riskmetric
+#'   assessments.
+#'
 #'
 #' @return A data.frame
 "v0.2.1_cran_20230621"
