880315f6cb6ae1a11c8382af4f68cbab81baeee2
aclark02-arcus <aclark02@arcusbio.com>
2025-09-30 14:10:21 +0000

    kick off new batch for sep 28, but automate things a bit more, plus consider saving pkg data as .parquet so it's more lightweight

---
+++ R/pull_risk.R
@@ -0,0 +1,44 @@
+
+
+
+#' Pull Risk Data
+#'
+#' A function to pull risk assessment & / or risk scores for all CRAN packages
+#'
+#' @param source character
+#' @param type character, either 'assessed' or 'scored'
+#' @param date character, of format 'YYYY-MM-DD',  'YYYYMMDD', or 'latest'
+#'
+#' @importFrom arrow read_parquet
+#' @importFrom stringr str_detect
+#'
+#' @examples
+#' pull_risk(type = "scored")
+#'
+#' @return A data.frame
+#' @export
+#'
+pull_risk <- function(
+  source = "cran", # anything else is unsupported right now
+  type = c("assessed", "scored")[1],
+  date = "latest" # can also pull by older date
+  ) {
+
+  if(!source %in% c("cran")) stop("Only 'cran' source is supported right now")
+  if(!date %in% c("latest", "2023-06-21", "2025-08-12", "2025-09-28")) {
+    stop("Only 'latest', '2023-06-21', '2025-08-12', or '2025-09-28' dates are supported right now")
+  }
+  if(stringr::str_detect(date, "-")) {
+    date <- gsub("-", "", date)
+  }
+  if(!type %in% c("assessed", "scored")) stop("Only 'assessed' or 'scored' type is supported right now")
+
+  # read in data
+  arrow::read_parquet(
+    # system.file(
+    file.path(
+      "data",
+      paste0(source, "_", type, "_", date, ".parquet")
+      # , package = "riskscore"
+  ))
+}
